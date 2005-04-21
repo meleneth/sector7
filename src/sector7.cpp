f53055a607f56d45af996220dd7a9d96560574ef
zyz <none>
2005-04-21 00:48:52 -0700

    esc kills client now
    darcs-hash:20050421074852-4fa27-fea61f44305eaeb7b94c74ee35287a5251621806.gz

--- src/sector7.cpp
+++ src/sector7.cpp
@@ -26,8 +26,66 @@ int main(int argc, char *argv[])
 //    player1 = new Player();
 
   //  entmgr->add_entity(player1);
+    
+    SDL_Event event;
+    Sint32 speed = 0;
+    Uint8 keys[1024];
+    Uint32 framecount = 0;
+
+    for (speed = 0; speed < 256; speed++)
+        keys[speed] = SDL_RELEASED;
+  
     while(!quit){
-        renderer->RenderFrame(sector);
+        while (SDL_PollEvent (&event) == 0)
+        {
+            renderer->RenderFrame(sector);
+            if (keys[SDLK_ESCAPE] == SDL_PRESSED)
+                quit = true;
+      //      handle_user_input();
+            SDL_Delay (5);
+            framecount++;
+        }
+        switch (event.type)
+        {
+            case SDL_ACTIVEEVENT:
+                {
+                    if (event.active.state & SDL_APPACTIVE)
+                    {
+                        if (event.active.gain)
+                        {
+                            printf ("App activated\n");
+                        }
+                        else
+                        {
+                            printf ("App iconified\n");
+                        }
+                    }
+                }
+                break;
+
+            case SDL_QUIT:
+                {
+                    printf ("Quit requested, quitting.\n");
+                    quit=true;
+                }
+                break;
+            case SDL_KEYUP:
+                //Keyboard handler
+                //      printf("-Value: %d\n",event.key.keysym.sym);
+
+                keys[event.key.keysym.sym] = 0;
+                break;
+
+            case SDL_KEYDOWN:
+                //              printf("Value: %d\n",event.key.keysym.sym);
+                keys[event.key.keysym.sym] = SDL_PRESSED;
+                break;
+
+        }
+
     }
+    SDL_Quit();
+    exit (0);
 }
+
 
