5dd9efee094f7f79603ecd7f9f2c40b5fbe78b09
meleneth <meleneth@icebox.sectorfour>
2005-05-07 00:39:38 -0700

    glOrtho should be based on innder_bound
    darcs-hash:20050507073938-57254-29398fe7cb258c9c821e61acb7affc8112fbe042.gz

--- src/engine/renderer.cpp
+++ src/engine/renderer.cpp
@@ -24,10 +24,10 @@ int Renderer::RenderFrame(Camera *camera)
 {
     static int reticle_angle = 0;
 
-    glOrtho (camera->position->x - camera->visible_area->width,
-             camera->position->x + camera->visible_area->width,
-             camera->position->y + camera->visible_area->height,
-             camera->position->y - camera->visible_area->height, 20, -20);
+    glOrtho (camera->position->x - camera->inner_bound->width,
+             camera->position->x + camera->inner_bound->width,
+             camera->position->y + camera->inner_bound->height,
+             camera->position->y - camera->inner_bound->height, 20, -20);
     ClearScreen();
     
     glColor4f(1, 1, 1, 1);
