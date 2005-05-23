90073a4a24c165576894149c42c98c6b10fa25d6
zyz <zyz@android>
2005-05-22 20:34:42 -0700

    added + and . to digits.png and trying to rectify that
    darcs-hash:20050523033442-94d54-c89550e4c759ce2e703354804a69b3822685d502.gz

--- src/engine/scoreboard.cpp
+++ src/engine/scoreboard.cpp
@@ -42,16 +42,16 @@ int ScoreBoard::DrawDigit(int digit)
 
     glBegin(GL_QUADS);
 
-    glTexCoord2f(.10 * (digit + 1), 1);
+    glTexCoord2f((1.0/12.0) * (digit + 1), 1);
     glVertex3f(-8, 8, 0.0);
 
-    glTexCoord2f(.10 * (digit + 1), 0.0);
+    glTexCoord2f((1.0/12.0) * (digit + 1), 0.0);
     glVertex3f(-8, -8, 0.0);
 
-    glTexCoord2f(.1 * digit, 0.0);
+    glTexCoord2f((1.0/12.0) * digit, 0.0);
     glVertex3f(8, -8, 0.0);
 
-    glTexCoord2f(.1 * digit, 1.0);
+    glTexCoord2f((1.0/12.0) * digit, 1.0);
     glVertex3f(8, 8, 0.0);
 
     glEnd();			// Done Drawing The Square
